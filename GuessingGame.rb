082679ba94bc631f93116ddf0e7b32f2722e70e7
Samuel Jubinville <samueljubinville119@gmail.com>
2024-02-17 19:28:47 -0500

    Added documentation, refactored code, updated ruby to version 3.0.6

--- GuessingGame.rb
+++ GuessingGame.rb
@@ -1,140 +1,28 @@
-# Guessing Game
+#GuessingGameUpdated
+require "tty-prompt"
 require 'artii'
 
-a = Artii::Base.new
-
-def veryEasy 
-    x = rand(11)
-    # puts 'Very easy mode number = ' + String(x)
-    num = 1
-    puts "Guess # #{num}"
+def play(guess_limit, maximum_guess_number)
+    x = rand(maximum_guess_number + 1)
     puts "Guess your number!"
-    guess = gets.chomp.to_i
-    until x == guess do
-        while guess > 10 || guess < 0 
-            puts "Please choose between 0 and 10"
-            guess = gets.chomp.to_i
-        end
-        puts "Chosen number is '#{guess}' "
-        if x < guess
-            puts "Your guess : #{guess} is greater than the number, try again"
-        elsif x > guess
-            puts "Your guess : #{guess} is lesser the number, try again"
-        end
-        num += 1
-    puts "Guess # #{num}"
-    guess = gets.chomp.to_i
-    end
-    puts "Congratulations! You guessed the right number :\n" + Artii::Base.new.asciify("#{x} with #{num} guesses!")
-end 
-
-def easy 
-    x = rand(11)
-    # puts 'Easy mode number = ' + String(x)
     guess = ''
-    puts "Guess your number!"
-    5.times do |num|
-    puts "guess # #{num + 1}"
-    guess = gets.chomp.to_i
-    while guess > 10 || guess < 0 
-        puts "Please choose between 0 and 10"
-        guess = gets.chomp.to_i
-    end
-        puts "Chosen number is '#{guess}' "
+    prompt = TTY::Prompt.new
+
+    guess_limit.times do |num|
+        guess = prompt.ask("guess # #{num + 1} :", convert: :int)
+        while guess > maximum_guess_number || guess < 0 
+            guess = prompt.ask("Please choose between 0 and #{maximum_guess_number} :", convert: :int)
+        end
         if x < guess
-            puts "Your guess : #{guess} is greater than the number, try again"
+            puts "Your guess number : #{guess} is greater than the number, try again"
         elsif x > guess
-            puts "Your guess : #{guess} is lesser the number, try again"
+            puts "Your guess number : #{guess} is lesser the number, try again"
         elsif x == guess
             puts "Congratulations! You guessed the right number :\n" + Artii::Base.new.asciify("#{x} with #{num + 1} guesses!")
             break
         end
     end
-    if guess != x 
-        puts "Game over. The number was"
-        puts Artii::Base.new.asciify("#{x}")
-        
-    end
-end 
 
-def medium
-    x = rand(101)
-    # puts 'Medium mode number = ' + String(x)
-    guess = ''
-    puts "Guess your number!"
-    6.times do |num|
-        puts "guess # #{num + 1}"
-        guess = gets.chomp.to_i
-        while guess > 100 || guess < 0 
-            puts "Please choose between 0 and 100"
-            guess = gets.chomp.to_i
-        end
-        puts "Chosen number is '#{guess}' "
-        if x < guess
-            puts "Your guess : #{guess} is greater than the number, try again"
-        elsif x > guess
-            puts "Your guess : #{guess} is lesser the number, try again"
-        elsif x == guess
-            puts "Congratulations! You guessed the right number :\n" + Artii::Base.new.asciify("#{x} with #{num + 1} guesses!")
-            break
-        end
-    end
-    if guess != x 
-        puts "Game over. The number was"
-        puts Artii::Base.new.asciify("#{x}")
-    end
-end 
-
-def hard
-    x = rand(501)
-    guess = ''
-    puts "Guess your number!"
-    # puts 'Hard mode number = ' + String(x)
-    8.times do |num|
-        puts "guess # #{num + 1}"
-        guess = gets.chomp.to_i
-        while guess > 500 || guess < 0 
-            puts "Please choose between 0 and 500"
-            guess = gets.chomp.to_i
-        end
-        puts "Chosen number is '#{guess}' "
-        if x < guess
-            puts "Your guess : #{guess} is greater than the number, try again"
-        elsif x > guess
-            puts "Your guess : #{guess} is lesser the number, try again"
-        elsif x == guess
-            puts "Congratulations! You guessed the right number :\n" + Artii::Base.new.asciify("#{x} with #{num + 1} guesses!")
-            break
-        end
-    end
-    if guess != x 
-        puts "Game over. The number was"
-        puts Artii::Base.new.asciify("#{x}")
-    end
-end 
-
-def veryHard
-    x = rand(1001)
-    # puts 'Very hard mode number = ' + String(x)
-    puts "Guess your number!"
-    guess = ''
-    9.times do |num|
-        puts "guess # #{num + 1}"
-        guess = gets.to_i
-        while guess > 1000 || guess < 0 
-            puts "Please choose between 0 and 1000"
-            guess = gets.chomp.to_i
-        end
-        puts "Chosen number is '#{guess}' "
-        if x < guess
-            puts "Your guess : #{guess} is greater than the number, try again"
-        elsif x > guess
-            puts "Your guess : #{guess} is lesser the number, try again"
-        elsif x == guess
-            puts "Congratulations! You guessed the right number :\n" + Artii::Base.new.asciify("#{x} with #{num + 1} guesses!")
-            break
-        end
-    end
     if guess != x 
         puts "Game over. The number was"
         puts Artii::Base.new.asciify("#{x}")
@@ -142,78 +30,62 @@ def veryHard
 end 
 
 def greet
+    prompt = TTY::Prompt.new
     puts "Welcome to a"
     puts Artii::Base.new.asciify("Number guessing game!")
-    puts "What is your name?"
-    name = gets.chomp
+    name = prompt.ask("What is your name?")
     puts Artii::Base.new.asciify("Welcome #{name}!")
-    puts "Type y whenever you are ready to start"
-    ready = gets.chomp
-    while ready != "y" && ready != "yes"
-        puts "Please type y or yes when you are ready"
-        ready = gets.chomp
-        end
-    if ready == "y" || ready == "yes"
-        game()
-    end
+    prompt.keypress("Press any key whenever you are ready to start.")
+    select_difficulty()
 end
         
 
-def game
-    puts "Select your difficulty between Very easy, easy, medium, hard or very hard!\n For explanations, type ?"
-    difficulty = gets.chomp
-    while difficulty != "very easy" && difficulty != "easy" && difficulty !=  "medium" && difficulty !=  "hard" && difficulty !=  "very hard" && difficulty !=  "?"
-        puts "Please select a valid difficulty. Type very easy, easy, medium, hard or very hard to select your difficulty or '?' for info"
-        difficulty = gets.chomp
-    end
-    if difficulty == "?" 
-        puts "Very easy mode lets you guess between 0 and 10, with unlimited guesses.
+def select_difficulty
+    maximum_guess_number = 0
+    guess_limit = -1
+
+    prompt = TTY::Prompt.new
+    difficulty = prompt.select("Select your difficulty between Very easy, easy, medium, hard or very hard!\n For explanations, select help",  %w(Very_easy easy medium hard very_hard help))
+    while difficulty == "help"
+        difficulty = prompt.select("Very easy mode lets you guess between 0 and 10, with unlimited guesses.
         Easy mode lets you guess between 0 and 10, with only 5 guesses.
         Medium mode lets you guess between 0 and 100, with only 6 guesses.
         Hard mode lets you guess between 0 and 500, with only 8 guesses.
         Very hard mode lets you guess between 0 and 1000, with only 9 guesses. 
-        Ready to try? Choose your difficulty!"
-        difficulty = gets.chomp
-        while difficulty != "very easy" && difficulty != "easy" && difficulty !=  "medium" && difficulty !=  "hard" && difficulty !=  "very hard"
-            puts "Please select a valid difficulty. Type very easy, easy, medium, hard or very hard to select your difficulty"
-            difficulty = gets.chomp
-        end
+        Ready to try? Choose your difficulty!", %w(Very_easy easy medium hard very_hard help))
     end
-        if difficulty == "very easy"
-            puts "Guess the number between 0-10"
-            veryEasy()
-        elsif difficulty == "easy"
-            puts "Guess the number between 0-10"
-            easy()
-        elsif difficulty == "medium"
-            puts "Guess the number between 0-100"
-            medium()
-        elsif difficulty == "hard"
-            puts "Guess the number between 0-500"
-            hard()
-        elsif difficulty == "very hard"
-            puts "Guess the number between 0-1000"
-            veryHard()
-        else 
-            puts "Please select your game's difficulty. Type ? for help."
+
+    case difficulty
+        when "Very_easy"
+            maximum_guess_number = 10
+            guess_limit = 99999
+        when "easy"
+            maximum_guess_number = 10
+            guess_limit = 5
+        when "medium"
+            maximum_guess_number = 100
+            guess_limit = 6
+        when "hard"
+            maximum_guess_number = 500
+            guess_limit = 8
+        when "very_hard"
+            maximum_guess_number = 1000
+            guess_limit = 9
     end
-    keepPlay()
+
+    puts "Guess the number between 0-#{maximum_guess_number}"
+    play(guess_limit, maximum_guess_number)
+    replay()
 end
 
-def keepPlay
-    puts "Want to retry? y/n"
-    continue = gets.chomp
-    while continue != "y" && continue != "yes" && continue != "n" && continue != "no"
-        puts "Please type y or n."
-        continue = gets.chomp
-    end
-
-    if continue == "y" || continue == "yes"
-        game()
-    elsif continue == "n" || continue == "no"
+def replay
+    prompt = TTY::Prompt.new
+    continue = prompt.yes?("Retry?")
+    if continue
+        select_difficulty()
+    else
         puts "Thank you for trying my guessing game."
     end
 end
-
 
 greet()
---
+++ readme.md
@@ -0,0 +1,154 @@
+<!-- Improved compatibility of back to top link: See: https://github.com/othneildrew/Best-README-Template/pull/73 -->
+<a name="readme-top"></a>
+<!--
+*** Thanks for checking out the Best-README-Template. If you have a suggestion
+*** that would make this better, please fork the repo and create a pull request
+*** or simply open an issue with the tag "enhancement".
+*** Don't forget to give the project a star!
+*** Thanks again! Now go create something AMAZING! :D
+-->
+
+
+
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+[![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/Spyro119/GuessingGame">
+    <img src="docs/logo.png" alt="Logo" height="100">
+  </a>
+
+<h3 align="center">GuessingGame</h3>
+
+  <p align="center">
+    Simple game where the goal is to guess a number before running out of attempts.
+    <br />
+    <a href="https://github.com/Spyro119/GuessingGame"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/Spyro119/GuessingGame">View Demo</a>
+    ·
+    <a href="https://github.com/Spyro119/GuessingGame/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/Spyro119/GuessingGame/issues">Request Feature</a>
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](/docs/Guessing_game-screenshot.png)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### Built With
+
+* ruby 3.0
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+### Prerequisites
+
+Be sure you have ruby installed on your system. 
+Else, simply follow this [guide](https://rvm.io/rvm/install).
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/Spyro119/GuessingGame.git
+   ```
+3. Install gems
+   ```sh
+   bundle install
+   ```
+4. simply run the file.
+   ```sh
+   ruby GuessingGame.rb
+   ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Samuel Jubinville-Baril - [@linkedin](https://www.linkedin.com/in/samuel-jubinville-baril-bbb5601a4/) <!-- - email@email_client.com -->
+
+Project Link: [https://github.com/Spyro119/GuessingGame](https://github.com/Spyro119/GuessingGame)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/Spyro119/GuessingGame.svg?style=for-the-badge
+[contributors-url]: https://github.com/Spyro119/GuessingGame/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/Spyro119/GuessingGame.svg?style=for-the-badge
+[forks-url]: https://github.com/Spyro119/GuessingGame/network/members
+[stars-shield]: https://img.shields.io/github/stars/Spyro119/GuessingGame.svg?style=for-the-badge
+[stars-url]: https://github.com/Spyro119/GuessingGame/stargazers
+[issues-shield]: https://img.shields.io/github/issues/Spyro119/GuessingGame.svg?style=for-the-badge
+[issues-url]: https://github.com/Spyro119/GuessingGame/issues
+[license-shield]: https://img.shields.io/github/license/Spyro119/GuessingGame.svg?style=for-the-badge
+[license-url]: https://github.com/Spyro119/GuessingGame/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://www.linkedin.com/in/samuel-jubinville-baril-bbb5601a4/
+[product-screenshot]: docs/Guessing_game-screenshot.png
